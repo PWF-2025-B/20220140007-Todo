68f414d2b3790f5ae032e98acdc5f204516c4f7e
Rafi Jadika <Rafi Jadika@LAPTOP-M1GUN5HA>
2025-03-16 20:29:12 +0700

    env, mvc, route, relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,10 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function user()
+
+    {
+        return $this->hasMany(user::class);
+    }
 }
