6478a7949ea56b4bc7ad1b288f8e94e323958090
Rafi Jadika <Rafi Jadika@LAPTOP-M1GUN5HA>
2025-05-24 12:30:41 +0700

    mailtrap

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,8 +6,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 
-class User extends Authenticatable
+
+
+
+class user extends Authenticatable implements MustVerifyEmail
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
---
+++ tugas10.md
@@ -0,0 +1,7 @@
+# Tugas 10
+
+![Alt text](screenshot/tugas10/dashboard.png)
+
+![Alt text](screenshot/tugas10/register.png)
+
+![Alt text](screenshot/tugas10/verity-email.png)
